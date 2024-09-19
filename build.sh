1f352b3f84bec24021173acc5dcd1c122774e4ba
Derek Scott <derekjscott@gmail.com>
2024-09-19 18:45:01 +1000

    another update of nixPkgs

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -e
 
-# Check if Composer is already installed
-if ! command -v composer &> /dev/null
-then
-    echo "Composer not found. Installing Composer..."
 # Install Composer manually
-    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-else
-    echo "Composer is already installed."
-fi
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # Install PHP dependencies
 composer install --no-dev --optimize-autoloader
