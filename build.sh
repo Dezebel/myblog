691fa7c10ff73332c8fb372347238b27c9af68c8
Derek Scott <derekjscott@gmail.com>
2024-09-19 18:10:51 +1000

    updated build.sh for composer install

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# Install Composer manually
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+set -e
+
+# Check if Composer is already installed
+if ! command -v composer &> /dev/null
+then
+    echo "Composer not found. Installing Composer..."
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+else
+    echo "Composer is already installed."
+fi
 
 # Install PHP dependencies
 composer install --no-dev --optimize-autoloader
