1d99670926f7ec65a7a4bbf67d21079939d8c9e9
Derek Scott <derekjscott@gmail.com>
2024-09-23 11:24:05 +1000

    updated database.php to default to mongodb only

--- config/database.php
+++ config/database.php
@@ -16,7 +16,8 @@ return [
     |
     */
 
-    'default' => env('MONGO_DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
+
 
     /*
     |--------------------------------------------------------------------------
